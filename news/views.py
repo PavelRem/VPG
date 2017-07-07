cf62044afaca86d1e86a75575c6e8bf4c6eff6d6
AndriiShostatskyi <shostatscky.andriy@yandex.ua>
2017-07-08 01:23:47 +0300

    /

--- news/views.py
+++ news/views.py
@@ -41,8 +41,8 @@ def news(request):
     print(NewsData.objects.order_by("pub_date")[:3].all())
     return render(request, 'index.html',  {'news': NewsData.objects.order_by("pub_date")[:3].all()})
 
-def fullread(request, id):
-    return render(request, 'News.html',  {'news_detail': NewsData.objects.get(id=1)})
+def fullread(request, id_news):
+    return render(request, 'News.html',  {'news_detail': NewsData.objects.get(id=id_news)})
 
 def login(request):
     return render(request, 'login.html',  {})
