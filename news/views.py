5662668e04349d965eabb3c5355411d873227961
AndriiShostatskyi <shostatscky.andriy@yandex.ua>
2017-07-08 18:10:36 +0300

    /

--- news/views.py
+++ news/views.py
@@ -88,7 +88,7 @@ def login(request):
     return render(request, 'login.html',  {})
 
 def search(request):
-    keywords = request.POST["search_input"]
+    keywords = request.POST.get('search_input', '')
     news_list = NewsData.objects.annotate(similarity=TrigramSimilarity('essance', keywords),).filter(similarity__gt=0.1).order_by('-similarity')
     paginator = Paginator(news_list, 6) # Show 25 contacts per page
     page = request.GET.get('page')
