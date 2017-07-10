d81537c86e1758daf006e28b26eed0b58a1cb766
AndriiShostatskyi <shostatscky.andriy@yandex.ua>
2017-07-10 19:35:10 +0300

    admin changes

--- news/views.py
+++ news/views.py
@@ -89,7 +89,7 @@ def login(request):
     return render(request, 'login.html',  {})
 
 def contacts(request):
-    return render(request, 'contacts.html',  {'contacts': Contacts.objects.all()[:1]})
+    return render(request, 'contacts.html',  {'info': Contacts.objects.all()[:1]})
 
 def partners(request):
     return render(request, 'partners.html', {'partners': Partners.objects.all(), 'partnerspage': [1]})
